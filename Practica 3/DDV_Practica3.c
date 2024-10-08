d57c0e99aa9a0135946e09cdbc30e6cade558d7d
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2024-10-08 10:50:44 -0600

    Mejoras

--- Practica 3/DDV_Practica3.c
+++ Practica 3/DDV_Practica3.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
+
+int comparaciones = 0;
+
+int es_numero_entero(const char *cadena)
+{
+    for (int i = 0; cadena[i] != '\0'; i++)
+    {
+        if (!isdigit(cadena[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 int busqueda_lineal(int arr[], int N, int x)
 {
-	int i; 
-    for ( i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
+        comparaciones++;
         if (arr[i] == x)
             return i;
+    }
     return -1;
 }
 
 int busqueda_binaria(int arr[], int low, int high, int x)
 {
-    while (low <= high) {
+    comparaciones++;
+    while (low <= high)
+    {
         int mid = low + (high - low) / 2;
 
+        comparaciones++;
         if (arr[mid] == x)
             return mid;
 
+        comparaciones++;
         if (arr[mid] < x)
             low = mid + 1;
         else
@@ -27,63 +47,70 @@ int busqueda_binaria(int arr[], int low, int high, int x)
     return -1;
 }
 
-int leer_archivo(int **arr, const char* nombre_archivo)
+int leer_archivo(int numero_elementos, int *arr)
 {
-    FILE *archivo = fopen(nombre_archivo, "r");
-    if (archivo == NULL) {
-        printf("No se pudo abrir el archivo %s\n", nombre_archivo);
-        return -1;
-    }
-
-    int capacidad = 1000;
     int i = 0;
-    *arr = (int *)malloc(capacidad * sizeof(int));
-    if (*arr == NULL) {
-        printf("Error al asignar memoria\n");
-        fclose(archivo);
-        return -1;
+    long file_size;
+    FILE *file = NULL;
+
+    srand(time(NULL));
+
+    printf("\nLeyendo el archivo numeros.txt\n");
+    file = fopen("numeros.txt", "r");
+    if (file == NULL)
+    {
+        printf("\nNo se puede abrir el archivo.\n");
+        return 0;
     }
 
-    while (fscanf(archivo, "%d", &(*arr)[i]) != EOF) {
+    fseek(file, 0L, SEEK_END);
+    file_size = ftell(file) / sizeof(int);
+    fseek(file, 0L, SEEK_SET);
+
+    if (file_size < numero_elementos)
+    {
+        printf("\nEl archivo no contiene suficientes elementos para la muestra solicitada.\n");
+        fclose(file);
+        return 0;
+    }
+
+    int indice_aleatorio = rand() % (file_size - numero_elementos);
+    fseek(file, indice_aleatorio * sizeof(int), SEEK_SET);
+
+    while (i < numero_elementos && fscanf(file, "%d", &arr[i]) != EOF)
+    {
         i++;
-        if (i >= capacidad) {
-            capacidad *= 2;
-            *arr = (int *)realloc(*arr, capacidad * sizeof(int));
-            if (*arr == NULL) {
-                printf("Error al reasignar memoria\n");
-                fclose(archivo);
-                return -1;
-            }
-        }
     }
 
-    fclose(archivo);
-    return i;  // Número de elementos leídos
+    fclose(file);
+
+    return i;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     int *arr = NULL;
-    int numero_elementos = leer_archivo(&arr, "numeros.txt");
-
-    if (numero_elementos <= 0) {
-        printf("Error al leer los numeros del archivo\n");
-        return 1;
-    }
-
+    int numero_elementos = 0;
     int x, opcion;
-    printf("Introduce el numero que quiere buscar: ");
-    scanf("%d", &x);
 
-    printf("\nSeleccione el tipo de busqueda que quiere aplicar:\n");
-    printf("1. Busqueda lineal\n");
-    printf("2. Busqueda binaria\n");
-    printf("Ingrese una opcin: ");
-    scanf("%d", &opcion);
+    if (argc == 4 && es_numero_entero(argv[1]) && es_numero_entero(argv[2]) && es_numero_entero(argv[3]))
+    {
+        numero_elementos = atoi(argv[1]);
+        opcion = atoi(argv[3]);
+        x = atoi(argv[2]);
 
-    switch (opcion) {
-        case 1: {
-            // Búsqueda lineal
+        if (numero_elementos <= 0)
+        {
+            printf("El numero de elementos debe ser mayor que cero.\n");
+            return 1;
+        }
+
+        arr = (int *)malloc(numero_elementos * sizeof(int));
+
+        switch (opcion)
+        {
+        case 1:
+        {
             int resultado_lineal = busqueda_lineal(arr, numero_elementos, x);
             if (resultado_lineal == -1)
                 printf("Numero no encontrado \n");
@@ -91,8 +118,8 @@ int main()
                 printf("Numero encontrado en la posicion %d \n", resultado_lineal);
             break;
         }
-        case 2: {
-            // Búsqueda binaria
+        case 2:
+        {
             int resultado_binario = busqueda_binaria(arr, 0, numero_elementos - 1, x);
             if (resultado_binario == -1)
                 printf("Numero no encontrado \n");
@@ -103,9 +130,57 @@ int main()
         default:
             printf("Opcion no valida, intente nuevamente \n");
             break;
+        }
     }
+    else
+    {
+        printf("Ingresa el numero de elementos sobre el cual quieres buscar: ");
+        scanf("%d", &numero_elementos);
+        if (numero_elementos <= 0)
+        {
+            printf("Error al leer los numeros del archivo\n");
+            return 1;
+        }
+
+        arr = (int *)malloc(numero_elementos * sizeof(int));
+
+        printf("Introduce el numero que quiere buscar: ");
+        scanf("%d", &x);
+
+        printf("\nSeleccione el tipo de busqueda que quiere aplicar:\n");
+        printf("1. Busqueda lineal\n");
+        printf("2. Busqueda binaria\n");
+        printf("Ingrese una opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+        {
+            int resultado_lineal = busqueda_lineal(arr, numero_elementos, x);
+            if (resultado_lineal == -1)
+                printf("Numero no encontrado \n");
+            else
+                printf("Numero encontrado en la posicion %d \n", resultado_lineal);
+            break;
+        }
+        case 2:
+        {
+            int resultado_binario = busqueda_binaria(arr, 0, numero_elementos - 1, x);
+            if (resultado_binario == -1)
+                printf("Numero no encontrado \n");
+            else
+                printf("Numero encontrado en la posicion %d \n", resultado_binario);
+            break;
+        }
+        default:
+            printf("Opcion no valida, intente nuevamente \n");
+            break;
+        }
+    }
+
+    printf("Numero de comparacciones: %d", comparaciones);
 
     free(arr);
     return 0;
 }
-
